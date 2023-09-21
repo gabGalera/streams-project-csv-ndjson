e2cd706d729d51d5401f205d68418d0e2eeed5aa
Gabriel Galera <gabgalera@hotmail.com>
2023-09-21 10:22:14 -0300

    set tasks

--- test/streamComponents/csvtondjson.test.js
+++ test/streamComponents/csvtondjson.test.js
@@ -3,4 +3,10 @@ import CSVToNDJSON from "../../src/streamComponents/csvtondjson.js";
 
 describe("CSV To NDJSON test suite", () => {
   it.todo("given a csv string it should return a ndjson string");
+  it.todo(
+    "it should work with strings that doesn't contain breaklines at the end"
+  );
+  it.todo(
+    "it should work with files that has breaklines at the beginning of the string"
+  );
 });
