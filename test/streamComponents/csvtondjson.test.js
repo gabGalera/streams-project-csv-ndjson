5ca166221e5193d5d35905f3d90621664809f750
Gabriel Galera <gabgalera@hotmail.com>
2023-09-21 10:33:25 -0300

    implements test: 'given a csv string it should return a ndjson string'

--- test/streamComponents/csvtondjson.test.js
+++ test/streamComponents/csvtondjson.test.js
@@ -1,8 +1,28 @@
-import { expect, describe, it } from "@jest/globals";
+import { expect, describe, it, jest } from "@jest/globals";
 import CSVToNDJSON from "../../src/streamComponents/csvtondjson.js";
 
 describe("CSV To NDJSON test suite", () => {
-  it.todo("given a csv string it should return a ndjson string");
+  it("given a csv string it should return a ndjson string", () => {
+    const csvString = `id,name,address\n01,gabriel,address01\n`;
+    const csvToJSON = new CSVToNDJSON({
+      delimiter: ",",
+      headers: ["id", "name", "address"],
+    });
+
+    const expected = JSON.stringify({
+      id: "01",
+      name: "gabriel",
+      address: "address01",
+    });
+    const fn = jest.fn();
+
+    csvToJSON.on("data", fn);
+    csvToJSON.write(csvString);
+    csvString.end();
+
+    const [current] = fn.mock.lastCall;
+    expect(current).toStrictEqual(expected);
+  });
   it.todo(
     "it should work with strings that doesn't contain breaklines at the end"
   );
